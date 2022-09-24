5cbf25797d6e2e6bf5e6146c04ecaa01c08979b3
lrosato <rosatoluca2001@gmail.com>
2022-09-24 11:51:36 -0300

    Class component added with a modal, header now changes, and css updated

--- src/components/Class.jsx
+++ src/components/Class.jsx
@@ -15,7 +15,8 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import "../App.css";
 import Rating from "./Rating";
 import Comment from "./Comment";
-
+import Modal from "./Modal";
+import {useState} from "react";
 
 
 const ExpandMore = styled((props) => {
@@ -36,6 +37,26 @@ export default function RecipeReviewCard(props) {
         setExpanded(!expanded);
     };
 
+    const [moreInfo, setMoreInfo] = useState(false);
+
+    React.useEffect(() => {
+
+    }, [moreInfo]);
+
+    const handleMoreInfo = () => {
+        if (moreInfo) {
+            return <Modal
+                nombre_clase = {props.nombre_clase}
+                materia_clase = {props.materia_clase}
+                frecuencia_clase = {props.frecuencia_clase}
+                costo_clase = {props.costo_clase}
+            />
+        } else {
+
+        }
+
+    }
+
     function getComments() {
         return props.comentarios_clase.map((comentario) => {
             return <Comment autor={comentario.alumno_id} comentario={comentario.comentario} />
@@ -44,7 +65,8 @@ export default function RecipeReviewCard(props) {
 
     return (
         <div className="class--container">
-            <Card sx={{ maxWidth: 345 }}>
+
+            <Card className="class--card">
                 <CardHeader
                     avatar={
                         <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -52,29 +74,39 @@ export default function RecipeReviewCard(props) {
                         </Avatar>
                     }
                     action={
-                        <IconButton aria-label="settings">
-                            <MoreVertIcon />
-                        </IconButton>
+                        <div onClick={() => setMoreInfo(!moreInfo)}>
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        </div>
                     }
                     title={props.nombre_clase}
                     subheader={props.fecha_clase}
                 />
+                <div className="class--more-info">
+                    {handleMoreInfo()}
+                </div>
                 <CardMedia
                     component="img"
 
                     image={props.imagen_clase}
                     alt={props.nombre_clase + " image"}
                 />
+
                 <div className="corners--image">
                     <h5 className="left--image">{props.duracion_clase} min</h5>
                     <h5 className="right--image">{props.tipo_clase}</h5>
+                </div>
+                <div className="class--more-info">
                 </div>
                 <CardContent>
                     <Typography variant="body2" color="text.secondary">
                         {props.descripcion_clase}
                     </Typography>
                 </CardContent>
+
                 <CardActions disableSpacing>
+
                     <Rating/>
                     <ExpandMore
                         expand={expanded}
